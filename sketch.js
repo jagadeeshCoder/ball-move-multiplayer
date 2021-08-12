d7b3039f7910a7a2ff30ada646fcfb8b86469953
jagadeeshCoder <74042387+jagadeeshCoder@users.noreply.github.com>
2021-08-12 13:24:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var ball;
+var database,position
+
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballposition= database.ref('ball/position')
+    ballposition.on("value",readposition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        WritePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        WritePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        WritePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        WritePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function WritePosition(x,y){
+
+    database.ref('ball/position').set({
+        x:position.x + x,
+        y:position.y + y
+    })
+}
+
+function readposition(data){
+    position=data.val()
+
+    ball.x=position.x
+    ball.y=position.y
+
+}
+
+function showError(){
+    console.log("Error in database")
+}
